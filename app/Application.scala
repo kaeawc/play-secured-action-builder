9f12c9943a2f5bb640dd2e24c7fe8fdebafeb1b4
Jason Pearson <jason.d.pearson@gmail.com>
2014-03-30 12:11:04 -0400

    added implementation of Secured with ActionBuilder

--- app/Application.scala
+++ app/Application.scala
@@ -5,7 +5,7 @@ import play.api.mvc._
 import scala.concurrent.{Future,ExecutionContext}
 import ExecutionContext.Implicits.global
 
-object Application extends Controller {
+object Application extends Controller with Security {
 
 /**
  * Normal actions in Play either expect an Action 
@@ -17,7 +17,7 @@ def normal = Action { Ok("") }
  * Secured, the way you have it implemented, is 
  * expecting an Action
  */
-def secured = Action { Ok("") }
+def secured = Secured { Ok("") }
 
 /**
  * And Action.async expects a Future of a SimpleResult,
@@ -27,10 +27,12 @@ def async = Action.async { Future { Ok("") } }
 
 /**
  * What you seem to be looking for is a Secured 
- * that takes a Future[SimpleResult]. To do that you
+ * that takes a Future[SimpleResult].  To do that you
  * should try to follow the example laid out in the
- * Play documentation for Action Composition.
+ * Play documentation for Action Composition.  If you
+ * implement Secured with ActionBuilder, then you can
+ * simply use the async modifier like so:
  */
-def securedAsync = Action.async { Future { Ok("") } }
+def securedAsync = Secured.async { Future { Ok("") } }
 
 }
--- app/Security.scala
+++ app/Security.scala
@@ -6,5 +6,16 @@ import scala.concurrent.{Future,ExecutionContext}
 import ExecutionContext.Implicits.global
 
 trait Security {
-    
+
+    class AuthenticatedRequest[A](val username: String, request: Request[A]) extends WrappedRequest[A](request)
+
+    object Secured extends ActionBuilder[AuthenticatedRequest] {
+      def invokeBlock[A](request: Request[A], block: (AuthenticatedRequest[A]) => Future[SimpleResult]) = {
+        request.session.get("username").map { username =>
+          block(new AuthenticatedRequest(username, request))
+        } getOrElse {
+          Future.successful(Results.Forbidden)
+        }
+      }
+    }
 }
