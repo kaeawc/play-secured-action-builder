5ff4d63a89c1a66188656e205a71983478f2d563
Jason Pearson <jason.d.pearson@gmail.com>
2014-03-30 12:29:55 -0400

    forgot to indent

--- app/Application.scala
+++ app/Application.scala
@@ -7,32 +7,32 @@ import ExecutionContext.Implicits.global
 
 object Application extends Controller with Security {
 
-/**
- * Normal actions in Play either expect an Action 
- * or a SimpleResult
- */
-def normal = Action { Ok("") }
+    /**
+     * Normal actions in Play either expect an Action 
+     * or a SimpleResult
+     */
+    def normal = Action { Ok("") }
 
-/**
- * Secured, the way you have it implemented, is 
- * expecting an Action
- */
-def secured = Secured { Ok("") }
+    /**
+     * Secured, the way you have it implemented, is 
+     * expecting an Action
+     */
+    def secured = Secured { Ok("") }
 
-/**
- * And Action.async expects a Future of a SimpleResult,
- *  but still returns an Action
- */
-def async = Action.async { Future { Ok("") } }
+    /**
+     * And Action.async expects a Future of a SimpleResult,
+     *  but still returns an Action
+     */
+    def async = Action.async { Future { Ok("") } }
 
-/**
- * What you seem to be looking for is a Secured 
- * that takes a Future[SimpleResult].  To do that you
- * should try to follow the example laid out in the
- * Play documentation for Action Composition.  If you
- * implement Secured with ActionBuilder, then you can
- * simply use the async modifier like so:
- */
-def securedAsync = Secured.async { Future { Ok("") } }
+    /**
+     * What you seem to be looking for is a Secured 
+     * that takes a Future[SimpleResult].  To do that you
+     * should try to follow the example laid out in the
+     * Play documentation for Action Composition.  If you
+     * implement Secured with ActionBuilder, then you can
+     * simply use the async modifier like so:
+     */
+    def securedAsync = Secured.async { Future { Ok("") } }
 
 }
